397ab38dc94b2bb4a9e2a2f5111a64a2259637e9
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2025-07-27 13:06:04 +0330

    Implement The Contact Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
 import Story from './components/Story'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
@@ -15,7 +16,9 @@ const App = () => {
       
       <Features />
       
-      <Story/>
+      <Story />
+      
+      <Contact/>
     </main>
   )
 }
